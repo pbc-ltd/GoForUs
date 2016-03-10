6a356319a6aa2a32688a73bee17b83ab413a90e0
James Summerill, Chris Greensides, Adam White <summergreenwhite@gmail.com>
2016-03-10 20:37:32 +0000

    rename project to actual name

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_cash_generator_session'
+Rails.application.config.session_store :cookie_store, key: '_go_for_yous_session'
