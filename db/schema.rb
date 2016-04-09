db95dbf58616f772423e249f9d5bd4f4f2fad1b9
Point Blank Coder <pointblankcoder@gmail.com>
2016-04-09 16:32:25 -0400

    Add is_read column to messages table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160409201226) do
+ActiveRecord::Schema.define(version: 20160409203152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 20160409201226) do
     t.string   "body"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.boolean  "is_read"
   end
 
   add_index "messages", ["conversation_id"], name: "index_messages_on_conversation_id", using: :btree
