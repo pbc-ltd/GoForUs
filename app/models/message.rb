03355b6a43906dc2327f3de1997905412c53f7c8
Point Blank Coder <pointblankcoder@gmail.com>
2016-04-11 07:29:23 -0400

    Send a notification to the receiver when a message is created

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,11 +5,22 @@ class Message < ActiveRecord::Base
 
   validates_presence_of :conversation, :conversation_id
 
+  after_create :send_notification
+
   def is_customer_message?
     !!customer
   end
 
   def is_partner_message?
     !is_customer_message
+  end
+
+  private
+  def send_notification
+    gcm_device_token = is_customer_message? ? conversation.partner.gcm_device_token : conversation.customer.gcm_device_token
+    Rpush::Gcm::Notification.new(
+      app: Rpush::Gcm::App.find_by_name('goforus_android'),
+      registration_ids: [gcm_device_token], data: { type: 'New Message', message: body }
+    ).save!
   end
 end
