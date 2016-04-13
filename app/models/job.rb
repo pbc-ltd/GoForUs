01de3fc495aa7376f9665871a8ac51aed06fd14b
Point Blank Coder <pointblankcoder@gmail.com>
2016-04-13 09:28:27 -0400

    Ensure we render json correctly?

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,4 +2,16 @@ class Job < ActiveRecord::Base
   belongs_to :partner
   belongs_to :customer
   belongs_to :order
+
+  def to_json
+    {
+      id: id,
+      order_id: order_id,
+      partner_id: partner_id,
+      customer_id: customer_id,
+      accepted: accepted,
+      declined: declined,
+      responded_to: responded_to
+    }.to_json
+  end
 end
