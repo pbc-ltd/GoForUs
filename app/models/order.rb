0dc74425bb6b40c56182caf3b3c47b39999c9506
Point Blank Coder <pointblankcoder@gmail.com>
2016-04-14 10:26:09 -0400

    Send a down data to the partner once the order has been created

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,37 @@ class Order < ActiveRecord::Base
 
   has_many :order_items
   has_many :items, through: :order_items
+
+
+  after_create :send_gcm_message
+
+  def to_json
+    to_hash.to_json
+  end
+
+  def to_hash
+    {
+      id: id,
+      accepted: accepted,
+      partner_id: partner_id,
+      customer_id: customer_id,
+      dropoff_location_lat: dropoff_location_lat,
+      dropoff_location_lng: dropoff_location_lng,
+      pickup_location_lat: pickup_location_lat,
+      pickup_location_lng: pickup_location_lng,
+      final_cost: final_cost,
+      estimated_cost: estimated_cost,
+      in_progress: in_progress,
+      conversation_id: conversation_id
+    }
+  end
+
+
+  private
+  def send_gcm_message
+    Rpush::Gcm::Notification.new(
+      app: Rpush::Gcm::App.find_by_name('goforus_android'),
+      registration_ids: [partner.gcm_device_token], data: { type: 'New Order', order: self.to_json }
+    ).save!
+  end
 end
